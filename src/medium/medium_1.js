926123afcd944506adf000f7319e64753c54d578
ellis <smellis@live.unc.edu>
2021-11-09 16:01:39 -0500

    com

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,6 +26,9 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
+
+    array.sort( function(a,b) {return a - b;} );
+
     var median = 0;
     var middleIndex = Math.floor(array.length / 2);
     if (array.length%2 == 0) {
