a498305674177488980ea3dccb6e29edade6e3aa
ellis <smellis@live.unc.edu>
2021-11-16 10:01:38 -0500

    someEven

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-import {moreStats} from "./medium/medium_2.js";
-var poop = moreStats;
-console.log(poop.avgMpgByYearAndHybrid);
+import {searchHighPower, searchMpg, searchByYear, searchName} from "./medium/medium_3.js";
+import mpg_data from "./medium/data/mpg_data.js";
+import {repeat} from "./spicy/spicy_9.js";
+function fn(x) {
+    return x;
+}
+var paramArray = ["poop", "caca"];
+console.log(repeat(fn, 3, paramArray[0]));
